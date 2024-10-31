4e1b900c7f3f5542ed42736548597899ca16e2f5
calummanleyy <cmanley6@my.bcit.ca>
2024-10-31 14:24:52 -0700

    Update problem.js

--- problem.js
+++ problem.js
@@ -8,8 +8,27 @@ Create a function that simulates a simple password checking system. The function
 
 Invoke examples of the function that fullfil each condition listed above. Feel free to work locally using node, and then paste your code into this file when it runs.
 
-CHALLENGE
-- If the password set is "forgot" or "reset", aside from "Access Granted", also log: "This password you set should not be used because it glitches the system".
-- If the password is less than 5 characters, log out "Your password is too short!".
-*/
+//code is down here vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+let password= "Monkey"
+let userInput= "monkey"
+
+//first create/name function
+
+function passwordCheck(password, userInput)
+//set paramiters as password and users input to compare the input to the password
+if (userInput === password) {
+    console.log ("Access Granted!") //if user input is equal to password, access is granted
+} else {
+    console.log ("Access Denied!") //if user inputs anything not equal to the password, access is denied
+}
+
+if (userInput === "forgot") { //if user input is equal to forgot, displays hint
+    console.log ("Here is a hint") }
+
+if (userInput === "reset") { //if user input is equal to reset, displays reset options
+    console.log("Let's reset your account")
+}
+
+//i tried my best happy halloween!!!
 
